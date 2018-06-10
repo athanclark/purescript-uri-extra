38beb9b05f66f71c8fae2a4b72bd26552d0e7179
Athan Clark <athan.clark@gmail.com>
2018-06-10 11:24:25 -0600

    improving, need to parse hashtag properly somehow I think

--- src/Data/URI/Location.purs
+++ src/Data/URI/Location.purs
@@ -1,18 +1,22 @@
 module Data.URI.Location where
 
 import Prelude
-import Data.Either (Either)
 import Data.Maybe (Maybe (..), maybe)
+import Data.Either (Either (..))
 import Data.URI (URIPathAbs, Query, Fragment, Scheme, Authority, HierarchicalPart (..), URI (..))
 import Data.URI.Query as Query
 import Data.URI.Fragment as Fragment
 import Data.URI.Path (parseURIPathAbs)
 import Data.URI.Path as URIPath
-import Data.Generic (class Generic)
-import Text.Parsing.StringParser (Parser)
+import Data.Generic (class Generic, gEq)
+import Data.Argonaut (class EncodeJson, class DecodeJson, encodeJson, decodeJson, fail)
+import Data.NonEmpty (NonEmpty (..))
+import Data.Path.Pathy.Gen (genAbsFilePath, genAbsDirPath)
+import Text.Parsing.StringParser (Parser, runParser)
 import Text.Parsing.StringParser.Combinators (optionMaybe)
 import Text.Parsing.StringParser.String (eof)
 import Test.QuickCheck (class Arbitrary, arbitrary)
+import Test.QuickCheck.Gen as QC
 
 
 data Location = Location URIPathAbs (Maybe Query) (Maybe Fragment)
@@ -20,16 +24,34 @@ data Location = Location URIPathAbs (Maybe Query) (Maybe Fragment)
 derive instance genericLocation :: Generic Location
 
 instance arbitraryLocation :: Arbitrary Location where
-  arbitrary = Location <$> arbitrary <*> arbitrary <*> arbitrary
+  arbitrary = Location <$> arbitraryURIPathAbs <*> arbitrary <*> arbitrary
+    where
+      arbitraryURIPathAbs = QC.oneOf $ NonEmpty
+        (Left <$> genAbsDirPath) [Right <$> genAbsFilePath]
+
 
 instance eqLocation :: Eq Location where
-  eq (Location p1 q1 f1) (Location p2 q2 f2) = p1 == p2 && q1 == q2 && f1 == f2
+  eq = gEq
+
+instance showLocation :: Show Location where
+  show = printLocation
+
+instance encodeJsonLocation :: EncodeJson Location where
+  encodeJson x = encodeJson (printLocation x)
+
+instance decodeJsonLocation :: DecodeJson Location where
+  decodeJson json = do
+    s <- decodeJson json
+    case runParser parseLocation s of
+      Left e -> fail (show e)
+      Right x -> pure x
 
 class ToLocation sym where
   toLocation :: sym -> Location
 
 class FromLocation sym where
   fromLocation :: Location -> Either String sym
+
 
 
 printLocation :: Location -> String
